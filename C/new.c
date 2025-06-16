e19cd6e1b711fb4352fdf7436db163f31496e5ab
KoushikScripts <kauschik@yahoo.com>
2025-06-16 11:26:17 +0530

    Practice: 2025-06-16 11:26:17

--- C/new.c
+++ C/new.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
 int main(){
-  int num = 10;
-  printf("Hello World\n");
-  printf("%d",num);
+  
+  char name[60];
+  printf("Enter your name  :%d");
+  scanf("%d", name);
+  printf("Your name is :%d\n", name);
+  
+  int num;
+  printf("Enter a num: ");
+  scanf("%d", &num);
+  printf("Your age is : %d",num);
 }
---
+++ CPP/new.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string fName;
+    string lName;
+    cout<<"Enter your first name:"<<endl;
+    cin>>fName;
+    cout<<"enter your last name: "<<endl;
+    cin>>lName;
+    cout<<"Hello "<<fName<<" "<<lName<<endl;
+    return 0;
+}
